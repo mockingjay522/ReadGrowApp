cf474a54d79a76b8dc787210c53673b67a6f087c
Kritika <kbhandari522@gmail.com>
2022-03-07 00:05:09 -0800

    Version 1.0.0

---
+++ app/src/main/java/com/example/readgrow/SignUpActivity1.java
@@ -0,0 +1,18 @@
+package com.example.readgrow;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up1);
+    }
+}
