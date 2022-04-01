87245ab5e5d640f14659a1516b7bbcabaaf07718
Andrew-shanan <andrew.shanan@hotmail.com>
2022-03-31 17:02:36 -0700

    the home page, getting the user name and spotted in (hi user textview) is done

--- app/src/main/java/com/example/readgrow/HomePage.java
+++ app/src/main/java/com/example/readgrow/HomePage.java
@@ -3,24 +3,34 @@ package com.example.readgrow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
 
-    private Button addOrUpdate;
-    private Button findBook;
-    private Button readingTracker;
+     Button addOrUpdate,findBook,readingTracker;
+     TextView userNameHoder; // (Hi User)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        // define controllers
         addOrUpdate = findViewById(R.id.home_addBook);
         findBook = findViewById(R.id.home_findBook);
         readingTracker = findViewById(R.id.home_readingTracker);
+        userNameHoder = findViewById(R.id.home_text);
+
+        // get the user name from SharedPreferences to be spotted in home_text (Hi User)
+        SharedPreferences shareFromLogin = PreferenceManager.getDefaultSharedPreferences(this);
+        userNameHoder.setText(shareFromLogin.getString("userName",""));
+
+
 
         addOrUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
